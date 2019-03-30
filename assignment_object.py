6449390e8ba8af3f6170388be9fa28a6aa370ca4
safeer1999 <safeerahmed.v@gmail.com>
2019-03-30 15:56:03 +0530

    Update assignment_object.py

--- assignment_object.py
+++ assignment_object.py
@@ -1,6 +1,6 @@
 class Assignment:
   
-  def __init__(self, time_interval=None, event=None, score=0, self.location=None) :
+  def __init__(self, time_interval=None, event=None, score=0, location=None) :
     self.location = location
     self.time_interval = time_interval
     self.event =  event

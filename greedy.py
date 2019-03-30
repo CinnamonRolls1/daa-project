63647b07b3ae93c407f2dd73e4c22ec918173a41
Chinmayan Pradeep <chinmayanks@gmail.com>
2019-03-30 13:12:38 +0530

    remove_assignment V2.0

--- greedy.py
+++ greedy.py
@@ -6,9 +6,8 @@ def remove_assignment(A,best_assignment):
 
 	#removing any clashing assignments
 	for assignment in A:
-		if assignment.location==best_assignment.location and assignment.time_interval==best_assignment.time_interval:
+		if (assignment.location==best_assignment.location and assignment.time_interval==best_assignment.time_interval) or assignment.name==best_assignment.name:
 			A.remove(assignment)
-
 
 def update_assignment(A,best_assignment):
 	pass

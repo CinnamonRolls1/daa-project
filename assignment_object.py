945da81281844afc1631a632766f919409c67c37
safeer1999 <safeerahmed.v@gmail.com>
2019-04-01 00:43:56 +0530

    Add files via upload
    #8 is completed

--- assignment_object.py
+++ assignment_object.py
@@ -1,9 +1,11 @@
-class Assignment:
-  
-  def __init__(self, time_interval=None, event=None, score=0, location=None, valid=True) :
-    self.location = location
-    self.time_interval = time_interval
-    self.event =  event
-    self.score = score
-    self.valid = valid
+class Assignment :
+
+	def __init__(self, time_interval = None, event = None, score = 0, location = None, valid = True):
+		
+		self.time_interval = time_interval
+		self.event = event
+		self.score = score
+		self.location = location
+		self.valid = valid
+		
     

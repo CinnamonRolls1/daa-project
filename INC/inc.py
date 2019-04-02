d9038b4111d6a672c874a149920f0ca0e83346d8
Safeer <safeerahmed.v@gmail.com>
2019-04-03 03:45:21 +0530

    #13 is done and execution_inc.py can be used for testing their respective codes, it has test values

--- INC/inc.py
+++ INC/inc.py
@@ -1,6 +1,6 @@
 #------------------------------------IMPORT LIBRARIES--------------------------------------------
 from assignment_object import Assignment
-from assignment_object import INC_traits
+from assignment_object import List_timeInt
 from itertools import product
 from operator import attrgetter
 #-------------------------------------------------------------------------------------------------
@@ -29,28 +29,118 @@ class SES :
 		self.A = []
 		self.S = []
 
-		self.L_i = []
+		self.L_i = [List_timeInt(i) for i in self.T]
+		self.M = [Assignment() for i in self.T]
 		self.bound = 0
-		self.M = []
 
 	#-------------------GENERATE ASSIGNMENT LIST-------------------------------
 	
-	def generate_assignment() : # line 1-5
-		pass
+	def generate_assignment(self) : # line 1-5 - generates all possible assignments and initializes the M list
 
+		events = list(range(len(self.E)))
+		time_intervals = list(range(len(self.T)))
+
+
+		c=list(product(events,time_intervals))
+
+
+
+		for i in c :
+
+			t_a_e = Assignment(i[1],i[0], location= self.location[i[0]])
+			self.A.append(t_a_e)
+
+			self.L_i[t_a_e.time_interval].l.append(t_a_e)
+
+
+
+		self.assign_score()
+
+
+	def insert_M(self, t_a_e) :
+
+		#print("inside insert_M")
+
+		if t_a_e.score > self.M[t_a_e.time_interval].score :
+			#print("inserted", t_a_e.score)
+			self.M[t_a_e.time_interval] = t_a_e
+
+
+	def assign_score(self) :
+
+		for i  in self.A :
+			i.score = self.score(i.event, i.time_interval, self.S+[i])
+			self.insert_M(i)
+
+		
 	#-------------------------------------------------------------------------
 
 
 	#-------------------SCORE CALCULATION------------------
 	
-	def score(event, time_interval, S) :
-		pass
+	def score(self, event, time_interval, S) :
+		net_score = 0
 
-	def prob_e_t_u(event, time_interval, u, S) :
-		pass
+		for u in range(len(self.U)) :
 
-	def update_score(assignment,best_assignment) :
-		pass
+			p = self.prob_e_t_u(event, time_interval, u, S)
+
+			net_score += p
+
+		return net_score
+
+	def prob_e_t_u(self, event, time_interval, u, S) :
+
+		#print("for user ",U[u])
+
+		mu_u_e = self.mu_E[u][event]
+
+		#print("mu_u_e: ", mu_u_e)
+
+		sigma_u_t = self.sigma[u][time_interval]
+
+		#print("sigma_u_t: ",sigma_u_t)
+
+		mu_u_c = 0
+		for c in range(len(self.mu_C[u])) :
+
+			if c == time_interval :
+				mu_u_c += self.mu_C[u][c]
+
+		#print("mu_u_c: ",mu_u_c)
+
+		mu_u_p = 0
+		for p in S :
+
+			if p.time_interval == time_interval :
+				mu_u_p += self.mu_E[u][p.event]
+
+		#print("mu_u_p: ",mu_u_p)
+
+		p = sigma_u_t * (mu_u_e / (mu_u_c + mu_u_p))
+
+		#print("p: ",p)
+
+		#print()
+
+		return p
+
+	def update_score(self,assignment,best_assignment) :
+
+		new_score = 0 
+		old_score = 0
+
+		new_S = self.S + [assignment]
+
+		for i in new_S :
+			new_score += self.score(i.event, assignment.time_interval, new_S)
+
+		for i in S :
+			old_score += self.score(i.event, assignment.time_interval, S)
+
+		#assignment.score = score(assignment.event,assignment.time_interval, S+[assignment]) - score(assignment.event, assignment.time_interval, S)
+		assignment.score = new_score - old_score
+		return assignment.score
 
 	#-------------------------------------------------------
 
@@ -58,7 +148,7 @@ class SES :
 
 	#-------------------------UPDATE LIST INTERVAL-----------------------------------
 
-	def update__L_i() : # line 9-10 
+	def update__L_i(self, top_assignment) : # line 9-10 
 		pass
 	#--------------------------------------------------------------------------------
 
@@ -66,22 +156,21 @@ class SES :
 
 	#-------------------------UPDATE TOP VALID UPDATED ASSIGN LIST -------------------
 
-	def update_M() : # line 11-15
+	def update_M(self, top_assignment) : # line 11-15
 		pass
 	#----------------------------------------------------------------------------------
 
 	#-------------------------FIND BOUND(Φ)-------------------
 
-	def get_bound() : # line 16
-		top_scorer=max(self.M, key=attrgetter('score'))
-		return top_scorer
+	def get_bound(self) : # line 16
+		pass
 
 	#--------------------------------------------------------
 
 
 	#------------------------UPDATE ASSIGNMENTS-----------------------------
 
-	def update_assignments() : # line 17 - 26
+	def update_assignments(self) : # line 17 - 26
 		pass
 	#-----------------------------------------------------------------------
 
@@ -89,21 +178,52 @@ class SES :
 
 	#-------------------------SELECT TOP VALID UPDATED ASSIGNMENT-------------
 
-	def get_top_assignment() : # line 7 '(similar to select_assignment()' from greedy.py)
-		max_assignment = Assignment()
-
-		for i in self.A:
-			if (i.score > max_assignment.score) and i.valid==True:
-				max_assignment = i
-
-		return max_assignment
-
+	def get_top_assignment(self) : # line 7 '(similar to select_assignment()' from greedy.py)
+		pass
 
 	#--------------------------------------------------------------------------
 	
 
 	#----------------------INC ALGORITHM------------------------------
-	def INC_algo() : # line 6-26
-		pass
+	def INC_algo(self) : # line 6-26
+	
+		for i in range(self.k) :	
+			top_assignment = self.get_top_assignment()
+
+			print("top_assignment: ",top_assignment)
+			self.status_log(self.A)
+
+			self.S.append(top_assignment)
+
+			self.update__L_i(top_assignment)
+
+			self.update_M(top_assignment)
+
+			self.bound = self.get_bound()
+
+			self.update_assignments()
 
 	#----------------------------------------------------------------
+
+	def status_log(self,assignment_list) :
+
+		print()
+		print()
+
+		print("Event  Time Interval  Score  Location  Validity")
+
+		for i in assignment_list :
+
+			if len(str(i.score)) >= 5 :
+
+				print(self.E[i.event], '   ', self.T[i.time_interval], '           ', '{:.5}'.format(str(i.score)), '', '{:7}'.format(i.location), ' ', i.valid)
+
+			else :
+				print(self.E[i.event], '   ', self.T[i.time_interval], '           ', '{:5}'.format(str(i.score)), '', '{:7}'.format(i.location), ' ', i.valid)
+
+
+		print()
+		print()
+
+
+

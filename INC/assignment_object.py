0c0933349dad1abbfdd3eb38c0ad0ffe81db609b
Safeer <safeerahmed.v@gmail.com>
2019-04-03 22:28:09 +0530

    assignment object file update. new data structure for L_i

--- INC/assignment_object.py
+++ INC/assignment_object.py
@@ -1,4 +1,5 @@
-class Assignment:
+
+class Assignment :
 
 	def __init__(self, time_interval = None, event = None, score = 0, location = None, valid = True, update = True):
 		
@@ -7,7 +8,15 @@ class Assignment:
 		self.score = score
 		self.location = location
 		self.valid = valid
-		self.update = update	
+		self.update = update
+
+class List_timeInt :
+
+	def __init__(self, time_interval = None, l = [], update = True):
+		
+		self.time_interval = time_interval
+		self.l = l[:]
+		self.update = update
 
 		
     
